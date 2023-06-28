477436e85019f975d58d8233155ce4477fe66c28
FRITZ IRONHACK <weninger.fritz@gmail.com>
2023-06-28 17:10:50 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import HomePage from "./pages/HomePage";
 import ProjectListPage from "./pages/ProjectListPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import EditProjectPage from "./pages/EditProjectPage";
-
+import SignupPage from "./pages/SignupPage";  // <== IMPORT
+import LoginPage from "./pages/LoginPage";  // <== IMPORT
 function App() {
   return (
     <div className="App">
@@ -15,7 +16,11 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/projects" element={<ProjectListPage />} />
         <Route exact path="/projects/:projectId" element={<ProjectDetailsPage />} />
-        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />           
+        <Route exact path="/projects/edit/:projectId" element={<EditProjectPage />} />   
+            {/*    ADD    */}
+            <Route path="/signup" element={ <SignupPage /> } />   
+                  {/*    ADD    */}
+        <Route path="/login" element={ <LoginPage /> } />     
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { AuthProviderWrapper } from "./context/auth.context"; // <== IMPORT
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Router>
+    <AuthProviderWrapper>      {/*  <== ADD  */}
       <App />
+      </AuthProviderWrapper>     {/*  <== ADD  */}
     </Router>
   </React.StrictMode>
 );
